74073687a5fa397a65668086c202e913d8d6b705
Ahmed Gomaa <ag1386840@gmail.com>
2026-08-15 20:29:40 +0300

    fix vercel routing and public directory setup

--- api/index.php
+++ api/index.php
@@ -1,3 +1,24 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+define('LARAVEL_START', microtime(true));
+
+// Fast-serve static files if found
+$publicPath = __DIR__ . '/../public';
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+if ($uri !== '/' && file_exists($publicPath . $uri)) {
+    return false;
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
